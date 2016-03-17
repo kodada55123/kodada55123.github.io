5cde72de8e42d42ce4cbafb5d53cc085f67421be
junacess <a403176845643457@gmail.com>
2016-03-17 20:48:13 +0800

    123123
    123456789

--- html2/js/work.js
+++ html2/js/work.js
@@ -1,19 +1,20 @@
-window.addEventListener('WebComponentsReady', function () {
-  var board = document.getElementById('board'),
-      light = document.getElementById('light');
 
-  board.on('ready',function ready() {
-    var led = document.getElementById('led');
 
-    light.addEventListener('click', function() {
-      if(light.className == 'on'){
-        led.off();     // 讓 LED 熄滅
-        light.className = 'off';
-      }else{
-        led.on();     // 讓 LED 亮起
-        light.className = 'on';
-      }
-    }, false);
-  });
+ $("#button").click(function(){
+        $("#div1").fadeIn();
+        $("#div2").fadeIn("slow");
+        $("#div3").fadeIn(3000);
+        $("#div4").fadeIn(5000);
+    });
+$(document).ready(function()
+{
+  $top_height=$("div[data-role=header]").height();
+  $bottom_height=$("div[data-role=footer]").height();
+  $body_height=$(window).height()-$top_height-$bottom_height;
 
-}, false);
+  
+  $body_height=$body_height-10;
+  $body_height=$body_height+"px";
+  $("div[data-role=metro_body]").width("100%").height($body_height);
+
+});
