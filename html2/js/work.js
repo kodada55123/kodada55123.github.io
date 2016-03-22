3059711488f4387058fb40847446dd41ed50e0e2
junacess <a403176845643457@gmail.com>
2016-03-22 17:50:11 +0800

    111
    1111

--- html2/js/work.js
+++ html2/js/work.js
@@ -1,20 +1,3 @@
 
 
- $("#button").click(function(){
-        $("#div1").fadeIn();
-        $("#div2").fadeIn("slow");
-        $("#div3").fadeIn(3000);
-        $("#div4").fadeIn(5000);
-    });
-$(document).ready(function()
-{
-  $top_height=$("div[data-role=header]").height();
-  $bottom_height=$("div[data-role=footer]").height();
-  $body_height=$(window).height()-$top_height-$bottom_height;
 
-  
-  $body_height=$body_height-10;
-  $body_height=$body_height+"px";
-  $("div[data-role=metro_body]").width("100%").height($body_height);
-
-});
